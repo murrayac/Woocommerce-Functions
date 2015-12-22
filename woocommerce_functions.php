fe1ed993bd71079cf572560c9d02f60e91dab92c
Murray Anderson-Clemence <me@murray.ac>
2015-12-23 09:26:00 +1000

    Update woocommerce_functions.php

--- woocommerce_functions.php
+++ woocommerce_functions.php
@@ -1,3 +1,17 @@
+// Display 24 products per page. Goes in functions.php
+add_filter( 'loop_shop_per_page', create_function( '$cols', 'return 18;' ), 20 );
+
+// Woocommerce Support
+add_theme_support( 'genesis-connect-woocommerce' );
+
+// Change number or products per row to 3
+add_filter('loop_shop_columns', 'loop_columns');
+if (!function_exists('loop_columns')) {
+	function loop_columns() {
+		return 3; // 3 products per row
+	}
+}
+
 // Replaces product short description with product content
 function woocommerce_template_product_description() {
 
@@ -16,4 +30,18 @@ function woo_remove_product_tabs( $tabs ) {
 
     return $tabs;
 
+}
+
+//* Change number of related products output
+function woo_related_products_limit() {
+  global $product;
+	
+	$args['posts_per_page'] = 3;
+	return $args;
+}
+add_filter( 'woocommerce_output_related_products_args', 'jk_related_products_args' );
+  function jk_related_products_args( $args ) {
+	$args['posts_per_page'] = 3; // 2 related products
+	$args['columns'] = 3; // arranged in 2 columns
+	return $args;
 }
