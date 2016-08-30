90a0b91a2ba9ed5e382cf65e358aa1abe2498292
Murray Anderson-Clemence <me@murray.ac>
2016-08-30 13:18:46 +1000

    Update woocommerce_functions.php

--- woocommerce_functions.php
+++ woocommerce_functions.php
@@ -103,3 +103,13 @@ function remove_pricing_from_category($price){
 		}
 
 }
+
+// Missing product redirect to product category
+add_filter( 'rewrite_rules_array', function( $rules )
+{
+    $new_rules = array(
+        'shop/([^/]*?)/page/([0-9]{1,})/?$' => 'index.php?product_cat=$matches[1]&paged=$matches[2]',
+        'shop/([^/]*?)/?$' => 'index.php?product_cat=$matches[1]',
+    );
+    return $new_rules + $rules;
+} );
